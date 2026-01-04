da9486bef942995dab2c922c709b0a63b087f82a
Jackrich06 <Jack.c.rich06@gmail.com>
2026-01-03 18:18:47 -0700

    Upgrade service worker to version 2 with new assets

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE = "count-trainer-v1";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+const CACHE = "count-trainer-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
@@ -15,3 +21,4 @@ self.addEventListener("fetch", (e) => {
     caches.match(e.request).then((cached) => cached || fetch(e.request).catch(() => caches.match("./index.html")))
   );
 });
+
